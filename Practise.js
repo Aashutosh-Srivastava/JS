44150f5c0997b9718f04a49a5fe2af27400c5d3d
Aashutosh Srivastava <srivastavaaashutosh1@gmail.com>
2025-01-14 23:13:47 +0530

    assignment 1

---
+++ Practise.js
@@ -0,0 +1,37 @@
+// // alert("You are in alert")
+// let name = prompt("Enter your name")
+// console.log(name)
+
+// q1
+// let num = prompt("Enter the number")
+// if(num%5 === 0)
+// {
+//     console.log("Number is divisible by 5")
+// }
+// else
+// {
+//     console.log("Number is not divisible by 5")
+// }
+
+// q2
+let marks = prompt("Enter the score")
+if(marks>=80 && marks<=100)
+{
+    console.log("Grade A")
+}
+else if(marks >=70 &&marks <=79)
+{
+    console.log("Grade B")
+}
+else if(marks >=60 && marks<=69)
+{
+    console.log("Grade C")
+}
+else if(marks >=50 && marks<=59)
+{
+    console.log("Grade D")
+
+}
+else{
+    console.log("Grade F")
+}
