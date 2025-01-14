9425f5b6bedbaa21c0c027c0725fbbe2ae558e76
Aashutosh Srivastava <srivastavaaashutosh1@gmail.com>
2025-01-14 23:00:13 +0530

    operators and conditionals

---
+++ conditonals.js
@@ -0,0 +1,41 @@
+// conditionals
+
+let age = 66
+// if(age>18)
+// {
+//     console.log("You are an adult" ) 
+// }
+// if(age<10)
+// {
+//     console.log("You are below 10")
+// }
+// else
+// {
+//     console.log("You are above 10")
+// }
+
+let num=60
+// if(num%2===0)
+// {
+//      console.log("even")
+// }
+// else{
+//     console.log("odd")
+// }
+
+// if(age<30)
+// {
+//     console.log("You are young")
+// }
+// else if(age<60)
+// {
+//     console.log("You are middle aged")
+// }
+// else
+// {
+//     console.log("You are old")
+// }
+
+// ternary operator
+// age>30?"adult":"not adult"
+console.log(age>30?"adult":"not adult")
---
+++ operators.js
@@ -0,0 +1,33 @@
+// Arithmetic operator
+let a = 5;
+let b = 3;
+// console.log("a+b = ",a+b)
+// console.log("a-b = ",a-b)
+// console.log("a*b = ",a*b)
+// console.log("a/b = ",a/b)
+// console.log("a%b = ",a%b) 
+// console.log("a**b = ",a**b)
+
+// unary operator
+// console.log("a++ =",a++)
+// console.log("b++ =",b++)
+// console.log("a-- =",a--)
+// console.log("b-- =",b--)
+
+// Assignment operator +=,-=,/=,*=,**=,%=
+// a+=4
+// console.log(a)
+
+// a-=3
+// console.log(a)
+
+// comparison operator ==,=== ,!=,!==,<=,>=,<,>
+
+// console.log("a==b",a==b)
+// console.log("a!==b",a!==b)
+
+// Logical operator && || !
+// console.log(a&&b)
+// console.log(a||b)
+// console.log(!b)
+
