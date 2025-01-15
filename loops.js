4394f02e6796b89146cb689534bda6a4d20feab4
Aashutosh Srivastava <srivastavaaashutosh1@gmail.com>
2025-01-15 13:44:09 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,32 @@
+// for(let i=0;i<=5;i++)         //loop  will print 1 to 6
+// {
+//     console.log(i+1);
+// }
+
+// console.log("loop has ended")
+
+//calculate sum of 1 to n
+// let sum=0
+// for(let i=1;i<=5;i++)
+// {
+//     sum+=i
+// }
+// console.log("Sum is =",sum)
+
+ 
+// while loop
+// let i=1
+// while(i<=5)
+// {
+//     console.log(i)
+//     i++
+// }
+
+// do while loop(atleast one time run)
+// let i =10;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=5)
+
+
---
+++ practiseLoop.js
@@ -0,0 +1,19 @@
+// Q1- print all even number from 0 to 100
+
+// for(let i =0;i<=100;i++)
+// {
+//     if(i%2===0)
+//     console.log(i);
+// }
+
+// Q2- guessing game
+// let num = 40;
+// let input = prompt("enter the number")
+// console.log(input)
+// for(let i =0;i<=50;)
+// while(input != num)
+// {
+//     let input2 = prompt("You guess the wrong number,enter number again")
+// }
+
+// console.log("congratulations,you entered the right number") 
