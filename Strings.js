8ad73cbf6e5712f85e8f282ef856401f0b231488
Aashutosh Srivastava <srivastavaaashutosh1@gmail.com>
2025-01-15 14:19:08 +0530

    Strings

---
+++ Strings.js
@@ -0,0 +1,37 @@
+// Strings
+
+let str = "  first string"
+
+// console.log(str.length)
+// console.log(str[0])
+
+// Template Literals
+// let sentance = `This is the template literal`
+// console.log(sentance)
+
+
+// using strings
+// let obj = {
+//     item:"Pen",
+//     price:10
+// };
+
+// console.log("The cost of",obj.item,"is",obj.price,"rupee");
+
+// // Template Literals -- way to embed expressions in strings
+// let sentance = `The cost of ${obj.item} is ${obj.price} rupee`
+// console.log(sentance)
+
+
+let str1 = str.toLowerCase()
+console.log(str1)
+let str2 = str.toUpperCase()
+console.log(str2)
+let str3 = str.trim()
+console.log(str3)
+
+// str.slice(Start,end)
+// str1.concat(str3)
+// str.replace(searchVal,newVal)
+console.log((str.replace("f","y")))
+// str.charAt(index)
