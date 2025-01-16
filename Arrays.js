ada02308d25bb9f93d2cbe06662e82c1ef705fde
Aashutosh Srivastava <srivastavaaashutosh1@gmail.com>
2025-01-16 22:32:03 +0530

    array

---
+++ Arrays.js
@@ -0,0 +1,13 @@
+let marks = [3,4,5];
+// console.log(marks)
+// console.log(marks.length) //property
+
+// //strings array
+// let fruits = ['apple','banana','cherry'];
+// console.log(fruits);
+// console.log(typeof marks);
+
+for(let i=0;i<marks.length;i++)
+{
+    console.log(marks[i]);
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+sole.log(marks)
+// console.log(marks.length) //property
+
+// //strings array
+// let fruits = ['apple','banana','cherry'];
+// console.log(fruits);
+// console.log(typeof marks);
